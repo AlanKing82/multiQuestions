960821ded0d11a7fddf0595b9d5796c67e54d52e
Alan King <alan.king@essentialskillz.com>
2016-06-08 10:23:48 +0100

    multiquestions

--- lang/eng/1_general_latest_courseware_and_currently_used/custom.js
+++ lang/eng/1_general_latest_courseware_and_currently_used/custom.js
@@ -9,7 +9,7 @@ var nextLessonButtonAlt = 'Launch next lesson';
 var endMessageAssessment = 'Click to start the %linked%';
 var congratulationLinkedAssessmentHeader = 'Risk Assessment';
 var congratulationLinkedSurveyHeader = 'Survey';
-var noOfScreens = 12;
+var noOfScreens = 8;
 var loadBookmarkText = 'Would you like to go to the last bookmarked screen in this lesson?';
 var currentLessonNo = 1;
 
